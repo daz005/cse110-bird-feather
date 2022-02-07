3918ffd38dc96647b2f629141344ce271b6fc669
Your Name <your_email@whatever.com>
2022-02-06 17:22:44 -0800

    bluetooth

--- app/src/main/java/edu/ucsd/cse110/lab5_nearby/FakedMessageListener.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_nearby/FakedMessageListener.java
@@ -1,32 +1,112 @@
 package edu.ucsd.cse110.lab5_nearby;
 
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.nearby.messages.MessageListener;
 import com.google.android.gms.nearby.messages.MessageListener;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 
 public class FakedMessageListener extends MessageListener{
 
     private final MessageListener messageListener;
     private final ScheduledExecutorService excutor;
 
-    public FakedMessageListener(MessageListener realMessageListener, int frequency, String messageStr){
+    public interface MessageHandler
+    {
+        public void onFound(Message message);
+        public void onLost(Message message);
+    }
 
-        this.messageListener = realMessageListener;
+    private List<String> messages_list =  new ArrayList<>();
+
+    public void broadCastMessage(String message){
+
+        this.messages_list.add(message);
+    }
+
+    public FakedMessageListener(MessageHandler messageHandler){
+        int frequency = 1;
+        this.messageListener =new MessageListener() {
+            @Override
+            public void onFound(@NonNull Message message) {
+                super.onFound(message);
+                messageHandler.onFound(message);
+            }
+
+            @Override
+            public void onLost(@NonNull Message message) {
+                super.onLost(message);
+                messageHandler.onLost(message);
+            }
+        };
+
         this.excutor = Executors.newSingleThreadScheduledExecutor();
 
         excutor.scheduleAtFixedRate(()->{
-            Message message = new Message( messageStr.getBytes(StandardCharsets.UTF_8) );
-            this.messageListener.onFound(message);
-            this.messageListener.onLost(message);
-        }, 0, frequency, TimeUnit.SECONDS);
+            while(!this.messages_list.isEmpty()) {
+                String messageStr = this.messages_list.remove(0);
+                Message message = new Message(messageStr.getBytes(StandardCharsets.UTF_8));
+                this.messageListener.onFound(message);
+                this.messageListener.onLost(message);
+            }
 
+        }, 0, frequency, TimeUnit.SECONDS);
     };
 
+
 }
+
+
+
+//
+//class FakedBluetoothListener extends MessageListener{
+//
+//    private final MessageListener messageListener;
+//    private final ScheduledExecutorService excutor;
+//
+//    public FakedBluetoothListener(MessageHandler messageHandler){
+//
+//        int frequency = 3;
+//        String messageStr = "hello Derek!";
+//
+//        this.messageListener =new MessageListener() {
+//
+//            @Override
+//            public void onFound(@NonNull Message message) {
+//                super.onFound(message);
+//
+//                messageHandler.onFound(message);
+//            }
+//
+//            @Override
+//            public void onLost(@NonNull Message message) {
+//                super.onLost(message);
+//
+//                messageHandler.onLost(message);
+//            }
+//        };
+//
+//
+//        this.excutor = Executors.newSingleThreadScheduledExecutor();
+//
+//        excutor.scheduleAtFixedRate(()->{
+//            Message message = new Message( messageStr.getBytes(StandardCharsets.UTF_8) );
+//            this.messageListener.onFound(message);
+//            this.messageListener.onLost(message);
+//        }, 0, frequency, TimeUnit.SECONDS);
+//
+//    };
+//
+//}
+
+
--- app/src/main/java/edu/ucsd/cse110/lab5_nearby/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab5_nearby/MainActivity.java
@@ -5,52 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.messages.Message;
-import com.google.android.gms.nearby.messages.MessageListener;
+//import com.google.android.gms.nearby.messages.Message;
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Lab5-Nearby";
-    private FakedMessageListener messageListener;
+
+public class MainActivity extends AppCompatActivity implements FakedMessageListener.MessageHandler {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MessageListener realListener =new MessageListener() {
-
-            @Override
-            public void onFound(@NonNull Message message) {
-                super.onFound(message);
-
-                Log.d(TAG, "Found message: " + new String(message.getContent()));
-            }
-
-            @Override
-            public void onLost(@NonNull Message message) {
-                super.onLost(message);
-
-                Log.d(TAG, "Lost sight of message: " + new String(message.getContent()));
-            }
-        };
-
-        //Fake receiving a message every three seconds:
-        this.messageListener = new FakedMessageListener(realListener, 3, "Hello world!");
+        //################ create it here #####################################
+        this.messageListener = new FakedMessageListener(this);
     };
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Nearby.getMessagesClient(this).subscribe(messageListener);
+    //#########################use it here ##################################
+    private static final String TAG = "Lab5-Nearby";
+
+    private FakedMessageListener messageListener;
+
+    // broadCast Message
+    public void onMyTestButtonClicked(View view) {
+
+        this.messageListener.broadCastMessage("This is from derek!");
     }
 
+    // found bluetooth message:
+    public void onFound(com.google.android.gms.nearby.messages.Message message){
 
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Nearby.getMessagesClient(this).unsubscribe(messageListener);
+        //####### your code here:
+        Log.d(TAG, "Found message: " + new String(message.getContent()));
     }
+
+    // lost bluetooth
+    public void onLost(com.google.android.gms.nearby.messages.Message message){
+
+        //####### your code here:
+        Log.d(TAG, "Lost sight of message: " + new String(message.getContent()));
+    }
+    //##############################################################################
+
 }
